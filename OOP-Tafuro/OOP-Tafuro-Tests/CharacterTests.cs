2079ef140276967c8b7eb83fc787ef876e71bbb4
stefanotafuro <stefano.tafuro@studio.unibo.it>
2021-07-09 16:34:32 +0200

    create class and test Character

---
+++ OOP-Tafuro/OOP-Tafuro-Tests/CharacterTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OOP_Tafuro;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Tafuro.Tests
+{
+    [TestClass()]
+    public class CharacterTests
+    {
+        Character testCharacter = new Character("Marty", 150, null);
+        Character testPlayerCharacter = new PlayerCharacter(new Character("Marty", 150, null));
+        Character testEnemyCharacter = new EnemyCharacter(new Character("Biff", 150, null), null);
+
+        [TestMethod()]
+        public void TestsCharacter()
+        {
+            Assert.AreEqual("Marty", testCharacter.Name);
+            Assert.AreEqual(150, testCharacter.Hp);
+            Assert.AreEqual(null, testCharacter.Weapon);
+        }
+
+        public void TestsPlayerCharacter()
+        {
+            Assert.AreEqual("Marty", testPlayerCharacter.Name);
+            Assert.AreEqual(150, testPlayerCharacter.Hp);
+            Assert.AreEqual(null, testPlayerCharacter.Weapon);
+        }
+
+        public void TestsEnemyCharacter()
+        {
+            Assert.AreEqual(null, testEnemyCharacter.DropWeapon);
+        }
+    }
+}
---
+++ OOP-Tafuro/OOP-Tafuro/Character.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace OOP_Tafuro
+{
+    public class Character
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        private int _hp;
+        public int Hp
+        {
+            get => _hp;
+            set => _hp = value;
+        }
+
+        private Weapon _weapon;
+        public Weapon Weapon
+        {
+            get => _weapon;
+            set => _weapon = value;
+        }
+
+        public Character(String name, int hp, Weapon weapon)
+        {
+            Name = name;
+            Hp = hp;
+            Weapon = weapon;
+        }
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+
+   public class EnemyCharacter : Character
+    {
+        internal Weapon DropWeapon { get; set; }
+        public EnemyCharacter(Character character, Weapon dropWeapon) : base(character.Name, character.Hp, character.Weapon)
+        {
+            DropWeapon = dropWeapon;
+        }
+    }
+
+    public class PlayerCharacter : Character
+    {
+        public PlayerCharacter(Character character) : base(character.Name, character.Hp, character.Weapon) { }
+    }
+
+
+
+}
